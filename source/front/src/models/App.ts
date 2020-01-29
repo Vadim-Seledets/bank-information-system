e2424cbf4c878ec155a089f2f193822d60932ef3
Vadim-Seledets <Vadik446s@gmail.com>
2020-01-30 00:51:03 +0300

    WIP: Main page styling

--- source/front/src/models/App.ts
+++ source/front/src/models/App.ts
@@ -1,10 +1,40 @@
-import { Stateful } from 'reactronic'
+import { Stateful, action } from 'reactronic'
 import { Api } from './Api'
+import { CustomerInfo } from './CustomerInfo'
+
+export class Tab extends Stateful{
+  constructor(public caption: string) {
+    super()
+  }
+}
 
 export class App extends Stateful {
   api = new Api('https://localhost:5001')
+  selectedCustomer?: {firstName: string, lastName: string} = undefined
+  customers = new Array<{firstName: string, lastName: string}>(
+    {firstName: 'Vadim', lastName: 'Seledets'},
+    {firstName: 'Arseni', lastName: 'Rynkevich'},
+    {firstName: 'Eguni', lastName: 'Dunin'},
+    {firstName: 'Max', lastName: 'Rock'},
+  )
+  currentTab?: Tab = undefined
+  tabs = new Array<Tab>(
+    new Tab('Customers'),
+    new Tab('Add a new customer'),
+  )
+  customerInfo = new CustomerInfo()
 
   constructor() {
     super()
+  }
+
+  @action
+  setSelectedCustomer(customer: {firstName: string, lastName: string}): void {
+    this.selectedCustomer = customer
+  }
+
+  @action
+  setCurrentTab(tab: Tab): void {
+    this.currentTab = tab
   }
 }
--- source/front/src/panels/AppWindow.tsx
+++ source/front/src/panels/AppWindow.tsx
@@ -5,24 +5,46 @@ import { App } from '../models/App'
 import { style } from './AppWindow.css'
 import { themes } from '../models/Theme'
 import { cx } from 'emotion'
+import { CustomerInfoPanel } from './CustomerInfoPanel'
 
 export function AppWindow(p: { app: App }): JSX.Element {
   return reactive(() => {
     const css = style.classes
+    const customerListPlace = p.app.selectedCustomer ? dim(3, 2, 6, 12) : dim(4, 2, 10, 12)
     return (
       <div className={css.main}>
-        <div className={css.menu} style={{...dim(1, 1, 3, 1)}}>
-          <img style={{height: "100%"}} src="assets/images/bank.png" onClick={() => themes.next()} />
-          <div onClick={() => {}}>Accounts</div>
-          <div onClick={() => {}}>Deposits</div>
-        </div>
-        <div style={{...dim(2, 2, 2, 2)}}>
-          <div className={cx(css.property)}>
-            <div className={cx(css.propertyCaption)}>First name</div>
-            <input className={cx(css.propertyInput)} type="text"/>
+        <div className={css.menu} style={{...dim(1, 1, 12, 1)}}>
+          <img style={{margin: '0 0.5em', width: '1.5em'}} src="assets/images/bank.png" onClick={() => themes.next()} />
+          <div>
+            Bank Information System
           </div>
         </div>
-        {/* <button onClick={}>Apply</button> */}
+        <div className={css.sidebar} style={{...dim(1, 2, 1, 12)}}>
+          {p.app.tabs.map((v, i) => (
+            <div key={i} className={cx(css.sidebarElement, css.clicable)}
+              is-selected={`${v === p.app.currentTab}`}
+              onClick={() => p.app.setCurrentTab(v)}>
+              {v.caption}
+            </div>
+          ))}
+        </div>
+        <div className={css.customerList} style={{...customerListPlace}}>
+          {p.app.customers.map((v, i) => (
+            <div key={i} className={css.customerListElement}
+              is-selected={`${v === p.app.selectedCustomer}`}
+              onClick={() => p.app.setSelectedCustomer(v)}>
+              {`${v.firstName} ${v.lastName}`}
+              <div className={'space'} />
+              <div className={cx('edit', 'action', 'las la-pen')} style={{marginRight: '0.5em'}} />
+              <div className={cx('edit', 'action', 'las la-trash')} />
+            </div>
+          ))}
+        </div>
+        {p.app.selectedCustomer && (
+          <div className={css.customerInfoPanel} style={{...dim(8, 2, 12, 12), background: 'red'}}>
+            <CustomerInfoPanel customerInfo={p.app.customerInfo} />
+          </div>
+        )}
       </div>
     )
   })
