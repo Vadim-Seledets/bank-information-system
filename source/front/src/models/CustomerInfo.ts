48c73c243e31ad38aad673f4a972ddc6638631f0
Vadim-Seledets <Vadik446s@gmail.com>
2020-01-30 01:58:50 +0300

    Comment out properties

--- source/front/src/models/CustomerInfo.ts
+++ source/front/src/models/CustomerInfo.ts
@@ -1,51 +1,51 @@
 import { Stateful, action } from 'reactronic'
 import { Customer } from './entities/Customer'
 
-export class CustomerInfoProperty extends Stateful {
-  constructor(
-    public name: string,
-    public type: string,
-    public validation?: string,
-  ) {
-    super()
-  }
+// export class CustomerInfoProperty extends Stateful {
+//   constructor(
+//     public name: string,
+//     public type: string,
+//     public validation?: string,
+//   ) {
+//     super()
+//   }
 
-  @action
-  validate(value: string): boolean {
-    return this.validation ? RegExp(this.validation).test(value) : true
-  }
-}
+//   @action
+//   validate(value: string): boolean {
+//     return this.validation ? RegExp(this.validation).test(value) : true
+//   }
+// }
 
 export class CustomerInfo extends Stateful {
   customer?: Customer
-  properties = Array<CustomerInfoProperty>(
-    new CustomerInfoProperty('Last name', 'text', '[A-Z][a-z]*([\'\- ][A-Z][a-z]+)*'),
-    new CustomerInfoProperty('First name', 'text', '[A-Z][a-z]*([\'\- ][A-Z][a-z]+)*'),
-    new CustomerInfoProperty('Middle name', 'text', '[A-Z][a-z]*([\'\- ][A-Z][a-z]+)*'),
-    new CustomerInfoProperty('Date of birth', 'date'),
-    new CustomerInfoProperty('Gender', 'radio'),
-    new CustomerInfoProperty('Passport series', 'text', '[A-Z]{2}'),
-    new CustomerInfoProperty('Passport number', 'text', '\d{7}'),
-    new CustomerInfoProperty('Issuing authority', 'text', '[A-Za-z ]+'),
-    new CustomerInfoProperty('Issued at', 'date'),
-    new CustomerInfoProperty('Id number', 'text', '[A-Z0-9]{14}'),
-    new CustomerInfoProperty('PlaceOfBirth', 'text', '[A-Za-z ]+'),
-    new CustomerInfoProperty('PlaceOfLiving', 'list'),
-    new CustomerInfoProperty('PlaceOfRegistration', 'text', '[A-Za-z ]+'),
-    new CustomerInfoProperty('Home phone number', 'text', '^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$'),
-    new CustomerInfoProperty('Mobile phone number', 'text', '^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$'),
-    new CustomerInfoProperty('Email', 'text', '^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$'),
-    new CustomerInfoProperty('Company', 'text', '[A-Za-z ]+'),
-    new CustomerInfoProperty('Position', 'text', '[A-Za-z ]+'),
-    new CustomerInfoProperty('City of registration', 'list'),
-    new CustomerInfoProperty('Address of registration', 'text', '[A-Za-z ]+'),
-    new CustomerInfoProperty('Marital status', 'list'),
-    new CustomerInfoProperty('Citizenship', 'list'),
-    new CustomerInfoProperty('Disability', 'list'),
-    new CustomerInfoProperty('Is retired', 'checkbox'),
-    new CustomerInfoProperty('Amount', 'money', '\d+'),
-    new CustomerInfoProperty('Is Liable For Military Service', 'checkbox'),
-  )
+  // properties = Array<CustomerInfoProperty>(
+  //   new CustomerInfoProperty('Last name', 'text', '[A-Z][a-z]*([\'\- ][A-Z][a-z]+)*'),
+  //   new CustomerInfoProperty('First name', 'text', '[A-Z][a-z]*([\'\- ][A-Z][a-z]+)*'),
+  //   new CustomerInfoProperty('Middle name', 'text', '[A-Z][a-z]*([\'\- ][A-Z][a-z]+)*'),
+  //   new CustomerInfoProperty('Date of birth', 'date'),
+  //   new CustomerInfoProperty('Gender', 'radio'),
+  //   new CustomerInfoProperty('Passport series', 'text', '[A-Z]{2}'),
+  //   new CustomerInfoProperty('Passport number', 'text', '\d{7}'),
+  //   new CustomerInfoProperty('Issuing authority', 'text', '[A-Za-z ]+'),
+  //   new CustomerInfoProperty('Issued at', 'date'),
+  //   new CustomerInfoProperty('Id number', 'text', '[A-Z0-9]{14}'),
+  //   new CustomerInfoProperty('PlaceOfBirth', 'text', '[A-Za-z ]+'),
+  //   new CustomerInfoProperty('PlaceOfLiving', 'list'),
+  //   new CustomerInfoProperty('PlaceOfRegistration', 'text', '[A-Za-z ]+'),
+  //   new CustomerInfoProperty('Home phone number', 'text', '^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$'),
+  //   new CustomerInfoProperty('Mobile phone number', 'text', '^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$'),
+  //   new CustomerInfoProperty('Email', 'text', '^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$'),
+  //   new CustomerInfoProperty('Company', 'text', '[A-Za-z ]+'),
+  //   new CustomerInfoProperty('Position', 'text', '[A-Za-z ]+'),
+  //   new CustomerInfoProperty('City of registration', 'list'),
+  //   new CustomerInfoProperty('Address of registration', 'text', '[A-Za-z ]+'),
+  //   new CustomerInfoProperty('Marital status', 'list'),
+  //   new CustomerInfoProperty('Citizenship', 'list'),
+  //   new CustomerInfoProperty('Disability', 'list'),
+  //   new CustomerInfoProperty('Is retired', 'checkbox'),
+  //   new CustomerInfoProperty('Amount', 'money', '\d+'),
+  //   new CustomerInfoProperty('Is Liable For Military Service', 'checkbox'),
+  // )
 
   @action
   setCustomer(customer: Customer): void {
