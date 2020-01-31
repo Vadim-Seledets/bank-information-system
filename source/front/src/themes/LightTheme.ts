a24153ae6a794c24c872cbb9584d96a4b7a1af36
Vadim-Seledets <Vadik446s@gmail.com>
2020-02-01 00:27:08 +0300

    Change applicationForeground color

--- source/front/src/themes/LightTheme.ts
+++ source/front/src/themes/LightTheme.ts
@@ -4,7 +4,7 @@ import { ITheme } from '../models/Theme'
 export class LightTheme extends Stateful implements ITheme {
   readonly name = "light"
   readonly applicationBackground = "#ffffff"
-  readonly applicationForeground = "#222222"
+  readonly applicationForeground = "#101010"
   readonly menuBackground = "#17233E"
   readonly sidebarBackground = "#202F53"
   readonly highlighter = "red"
