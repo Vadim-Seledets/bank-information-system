5f44dbe539e0c1726325af338e7111cea4a96ae8
Vadim-Seledets <Vadik446s@gmail.com>
2020-02-01 00:26:05 +0300

    Correct PlaceOfLiving model

--- source/front/src/models/entities/PlaceOfLiving.ts
+++ source/front/src/models/entities/PlaceOfLiving.ts
@@ -1,20 +1,17 @@
 import { Stateful } from "reactronic"
 
 export interface IPlaceOfLiving {
-  customerId: number
-  city: string
+  cityId: number
   address: string
 }
 
 export class PlaceOfLiving extends Stateful {
-  customerId: number
-  city: string
+  cityId: number
   address: string
 
   constructor(placeOfLiving: IPlaceOfLiving) {
     super()
-    this.customerId = placeOfLiving.customerId
-    this.city = placeOfLiving.city
+    this.cityId = placeOfLiving.cityId
     this.address = placeOfLiving.address
   }
 }
