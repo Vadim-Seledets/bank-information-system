f0f2b7f5a37d31d4d956903115936c698a776d34
Vadim-Seledets <Vadik446s@gmail.com>
2020-02-01 00:23:10 +0300

    Add Auxiliary model

---
+++ source/front/src/models/entities/Auxiliary.ts
@@ -0,0 +1,9 @@
+export class Auxiliary {
+  cities: Array<{id: number, name: string}> = []
+  countriesOfCitizenship: Array<{id: number, country: string}> = []
+  disabilities: Array<{id: number, description: string}> = []
+  currencies: Array<{id: number, code: string}> = []
+  maritalStatuses: Array<{id: number, description: string}> = []
+
+  static readonly Empty = new Auxiliary()
+}
