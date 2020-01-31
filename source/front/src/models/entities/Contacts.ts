4c3a4ec916cd38ba7871b9a2fff90d1e34f77dc0
Vadim-Seledets <Vadik446s@gmail.com>
2020-02-01 00:23:58 +0300

    Correct Contacts model

--- source/front/src/models/entities/Contacts.ts
+++ source/front/src/models/entities/Contacts.ts
@@ -1,27 +1,19 @@
 import { Stateful } from "reactronic"
-import { Customer } from "./Customer"
 
 export interface IContacts {
-  customerId: number
   email: string
   homePhoneNumber: string
   mobilePhoneNumber: string
-  customer: Customer
 }
-
 export class Contacts extends Stateful {
-  customerId: number
   email: string
   homePhoneNumber: string
   mobilePhoneNumber: string
-  customer: Customer
 
   constructor(contacts: IContacts) {
     super()
-    this.customerId = contacts.customerId
     this.email = contacts.email
     this.homePhoneNumber = contacts.homePhoneNumber
     this.mobilePhoneNumber = contacts.mobilePhoneNumber
-    this.customer = contacts.customer
   }
 }
