b3bc2bbed9daaf7728bf1c4a52499209c23ade92
Vadim-Seledets <Vadik446s@gmail.com>
2020-02-01 00:26:19 +0300

    Correct PlaceOfRegistration model

--- source/front/src/models/entities/PlaceOfRegistration.ts
+++ source/front/src/models/entities/PlaceOfRegistration.ts
@@ -1,24 +1,17 @@
 import { Stateful } from "reactronic"
-import { Customer } from "./Customer"
 
 export interface IPlaceOfRegistration {
-  customerId: number
-  city: string
+  cityId: number
   address: string
-  customer: Customer
 }
 
 export class PlaceOfRegistration extends Stateful {
-  customerId: number
-  city: string
+  cityId: number
   address: string
-  customer: Customer
 
   constructor(placeOfRegistration: IPlaceOfRegistration) {
     super()
-    this.customerId = placeOfRegistration.customerId
-    this.city = placeOfRegistration.city
+    this.cityId = placeOfRegistration.cityId
     this.address = placeOfRegistration.address
-    this.customer = placeOfRegistration.customer
   }
 }
