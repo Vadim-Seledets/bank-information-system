880039f7668e1e5a6d6e91b3e96608b4e839b8ae
Vadim-Seledets <Vadik446s@gmail.com>
2020-02-01 01:28:51 +0300

    Get rid of unused css classes

--- source/front/src/panels/CustomersPage.css.ts
+++ source/front/src/panels/CustomersPage.css.ts
@@ -5,47 +5,6 @@ import { themes } from '../models/Theme'
 export const style = restyle(() => {
   const theme = themes.active
   return {
-    main: css`
-      label: main;
-
-      background-color: ${theme.applicationBackground};
-      display: grid;
-      position: absolute;
-      top: 0;
-      left: 0;
-      bottom: 0;
-      right: 0;
-      overflow: hidden;
-      font-family: Calibri, Arial, Tahoma;
-      font-size: calc(16px + (24 - 16) * (100vw - 640px) / (1920 - 640));
-      font-weight: 300;
-      grid-template-columns: 2fr repeat(11, 1fr);
-      grid-template-rows: 1.5em 1.5em repeat(10, 1fr);
-
-      button {
-        padding: 0.1em;
-        margin: 0 0.25em;
-        min-width: 1.75em;
-        white-space: nowrap;
-      }
-
-      button:hover {
-        cursor: pointer;
-      }
-
-      button, input {
-        transition: background-color ease 0.5s, border-bottom ease 0.5s, box-shadow ease 0.5s;
-        box-shadow: 0 0 1px 0 rbga(127, 127, 127, 0.5);
-        padding: 0 0.2em;
-      }
-    `,
-
-    menu: css`
-      display: flex;
-      align-items: center;
-      background-color: ${theme.menuBackground};
-    `,
-
     actionPanel: css`
       display: flex;
       align-items: center;
@@ -66,80 +25,6 @@ export const style = restyle(() => {
       display: grid;
       grid-template-columns: repeat(12, 1fr);
       grid-template-rows: 1.5em repeat(10, 1fr);
-    `,
-
-    editPanelCaption: css`
-      color: ${theme.applicationForeground};
-      text-align: center;
-      align-self: center;
-    `,
-
-    actionsOnEditPanel: css`
-      display: flex;
-      align-self: center;
-      align-items: center;
-    `,
-    
-    editButton: css`
-      color: ${theme.applicationForeground};
-
-      &:hover {
-        color: #aaa;
-      }
-    `,
-
-    closeEditPanel: css`
-      color: ${theme.applicationForeground};
-    `,
-
-    sidebar: css`
-      display: flex;
-      flex-direction: column;
-      background: ${theme.sidebarBackground}
-    `,
-
-    sidebarElement: css`
-      display: flex;
-      align-items: center;
-      padding: 0.3em 0 0.25em 0.3em;
-      /* border-bottom: 1px solid grey; */
-
-      &:hover {
-        background: ${theme.applicationBackgroundDemmed};
-      }
-
-      &[is-selected=true] {
-        color: ${theme.applicationForeground};
-        background: ${theme.applicationBackground};
-      }
-
-      .icon {
-        margin-right: 0.5em;
-        font-size: 1.2em;
-      }
-
-      .caption {
-        font-size: 0.9em;
-      }
-    `,
-
-    editCustomerButton: css`
-      color: grey;
-      pointer-events: none;
-
-      &[is-enabled=true] {
-        color: white;
-      }
-    `,
-
-    clicable: css`
-      cursor: pointer;
-    `,
-
-    addCustomerButton: css`
-      color: ${theme.applicationForeground};
-      align-self: center;
-      justify-self: center;
     `,
 
     customerList: css`
@@ -178,10 +63,6 @@ export const style = restyle(() => {
           display: initial;
         }
       }
-    `,
-
-    customerInfoPanel: css`
-      display: flex;
     `,
   }
 })
