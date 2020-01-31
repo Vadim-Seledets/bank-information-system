5d7c4e8ac04cca2e02d536a01736f28e6c2625ba
Vadim-Seledets <Vadik446s@gmail.com>
2020-02-01 00:28:11 +0300

    AppWindow styling

--- source/front/src/panels/AppWindow.tsx
+++ source/front/src/panels/AppWindow.tsx
@@ -10,11 +10,12 @@ import { CustomerInfoPanel } from './CustomerInfoPanel'
 export function AppWindow(p: { app: App }): JSX.Element {
   return reactive(() => {
     const css = style.classes
-    const customerListPlace = p.app.selectedCustomer ? dim(3, 3, 6, 12) : dim(4, 3, 10, 12)
+    const customersPagePlace = p.app.selectedCustomer ? dim(3, 2, 6, 12) : dim(4, 2, 10, 12)
+    // const customerListAddButtonPlace = p.app.selectedCustomer ? dim(7, 2, 7, 2) : dim(11, 2, 11, 2)
     return (
       <div className={css.main}>
         <div className={css.menu} style={{...dim(1, 1, 12, 1)}}>
-          <img style={{margin: '0 0.5em', width: '1.5em'}} src="assets/images/bank.png" onClick={() => themes.next()} />
+          <img style={{margin: '0 0.5em', width: '1.2em'}} src="assets/images/bank.png" onClick={() => themes.next()} />
           <div>
             Bank Information System
           </div>
@@ -28,25 +29,36 @@ export function AppWindow(p: { app: App }): JSX.Element {
             </div>
           ))}
         </div>
-        <div className={css.customerList} style={{...customerListPlace}}>
-          {p.app.customers.map((v, i) => (
-            <div key={i} className={css.customerListElement}
-              is-selected={`${v === p.app.selectedCustomer}`}
-              onClick={() => p.app.setSelectedCustomer(v)}>
-              {`${i+1}. ${v.firstName} ${v.lastName}`}
-              <div className={'space'} />
-              <div className={cx('edit', 'action', 'las la-pen')} style={{marginRight: '0.5em'}} />
-              <div className={cx('edit', 'action', 'las la-trash')} />
+        <div className={css.customersPage} style={{...customersPagePlace}}>
+          <div className={css.actionPanel} style={{...dim(1, 1, 12, 1), color: 'grey'}}>
+            <div className='addNewCustomerButton'>
+              Add a New Customer
             </div>
-          ))}
+            <input className='search' type='text' placeholder='Find a Customer...'/>
+          </div>
+          <div className={css.customerList} style={{... dim(1, 2, 12, 12)}}>
+            {p.app.customers.map((v, i) => (
+              <div key={i} className={css.customerListElement}
+                is-selected={`${v === p.app.selectedCustomer}`}
+                onClick={() => p.app.setSelectedCustomer(v)}>
+                {`${i+1}. ${v.firstName}`}
+                <div className={'space'} />
+                <div className={cx('edit', 'action', 'las la-pen')} style={{marginRight: '0.5em'}} />
+                <div className={cx('edit', 'action', 'las la-trash')} />
+              </div>
+            ))}
+          </div>
         </div>
         {p.app.selectedCustomer && (
           <React.Fragment>
             <div className={css.caption} style={{...dim(8, 2, 11, 2)}}>
               Edit customer information
             </div>
-            <button className={css.editButton} style={{...dim(12, 2, 12, 2)}}>Edit</button>
-            <div className={css.customerInfoPanel} style={{...dim(8, 3, 12, 12), background: 'red'}}>
+            <div className={css.actionsOnEditPanel} style={{...dim(12, 2, 12, 2)}}>
+              <button className={css.editButton}>Edit</button>
+              <div className={cx(css.closeEditPanel, 'las la-times')}></div>
+            </div>
+            <div className={css.customerInfoPanel} style={{...dim(8, 3, 11, 12)}}>
               <CustomerInfoPanel customerInfo={p.app.customerInfo} />
             </div>
           </React.Fragment>
