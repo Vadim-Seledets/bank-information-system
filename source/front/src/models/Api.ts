4c0737b20d5d35b24a560f4664f4f71af9b41e81
Vadim-Seledets <Vadik446s@gmail.com>
2020-02-01 12:53:55 +0300

    Get rid of getAllCustomersInShortInfoModel method

--- source/front/src/models/Api.ts
+++ source/front/src/models/Api.ts
@@ -1,15 +1,7 @@
-import { Customer, ICustomer } from "./entities/Customer"
 import { Auxiliary } from "./entities/Auxiliary"
 
 export class Api {
   constructor(public baseUrl: string) {
-  }
-
-  async getAllCustomersInShortInfoModel(): Promise<Array<Customer>> {
-    const json = await fetch(`${this.baseUrl}/customers`)
-      .then(response => response.json())
-    const customers = json as ICustomer[]
-    return customers.map(c => new Customer(c))
   }
   
   async getAuxiliaryInfo(): Promise<Auxiliary> {
