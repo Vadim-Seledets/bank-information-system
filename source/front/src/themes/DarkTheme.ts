bac58d67638fb6202bf33994b9269e3f9a822a51
Vadim-Seledets <Vadik446s@gmail.com>
2020-02-06 22:54:15 +0300

    Change customerListHoveredRowBackground color

--- source/front/src/themes/DarkTheme.ts
+++ source/front/src/themes/DarkTheme.ts
@@ -17,6 +17,6 @@ export class DarkTheme extends Stateful implements ITheme {
   readonly customerSearchBackground = "#434341"
   readonly customerSearchBorderColor = "#727270"
   readonly customerListOddRowBackground = "#333332"
-  readonly customerListHoveredRowBackground = "#666662"
+  readonly customerListHoveredRowBackground = "#414141"
   readonly customerListHighlightedRowBackground = "#0e5ccd"
 }
