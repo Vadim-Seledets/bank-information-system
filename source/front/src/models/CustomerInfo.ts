308a0eb8ad1a5f785eaad2579e50f6220ff42a80
Vadim-Seledets <Vadik446s@gmail.com>
2020-02-07 21:00:22 +0300

    Use CustomersPage instead of App

--- source/front/src/models/CustomerInfo.ts
+++ source/front/src/models/CustomerInfo.ts
@@ -1,45 +1,45 @@
 import { Stateful, action, trigger } from 'reactronic'
 import { Auxiliary } from './customers/Auxiliary'
-import { App } from './App'
 import { Validation, PropertyValidator } from './Validation'
 import { CustomerKeys } from './customers/Customer'
+import { CustomersPage } from './CustomersPage'
 
 export class CustomerInfo extends Stateful {
-  app: App
+  customersPage: CustomersPage
   auxiliary: Auxiliary
   validation: Validation
 
-  constructor(app: App) {
+  constructor(customersPage: CustomersPage) {
     super()
-    this.app = app
+    this.customersPage = customersPage
     this.auxiliary = new Auxiliary()
     this.validation = new Validation(new Map<CustomerKeys, PropertyValidator>([
-      ['lastName', new PropertyValidator(this.app, 'lastName', /^([A-Z][a-z]*[\'\- ]?[A-Za-z]+)?$/)],
-      ['firstName', new PropertyValidator(this.app, 'firstName', /^([A-Z][a-z]*[\'\- ]?[A-Za-z]+)?$/)],
-      ['middleName', new PropertyValidator(this.app, 'middleName', /^([A-Z][a-z]*[\'\- ]?[A-Za-z]+)?$/)],
-      ['dateOfBirth', new PropertyValidator(this.app, 'dateOfBirth', /^\d{4}-\d{2}-\d{2}$/)],
-      ['gender', new PropertyValidator(this.app, 'gender')],
-      ['series', new PropertyValidator(this.app, 'series', /^([A-Z]{2})?$/)],
-      ['passportNumber', new PropertyValidator(this.app, 'passportNumber', /^(\d{7})?$/)],
-      ['issuingAuthority', new PropertyValidator(this.app, 'issuingAuthority', /^[A-Za-z' ]*$/)],
-      ['issuedAt', new PropertyValidator(this.app, 'issuedAt', /^\d{4}-\d{2}-\d{2}$/)],
-      ['idNumber', new PropertyValidator(this.app, 'idNumber', /^([A-Z0-9]{14})?$/)],
-      ['placeOfBirth', new PropertyValidator(this.app, 'placeOfBirth', /^[A-Za-z ]*$/)],
-      ['placeOfLivingCityId', new PropertyValidator(this.app, 'placeOfLivingCityId')],
-      ['placeOfLivingAddress', new PropertyValidator(this.app, 'placeOfLivingAddress')],
-      ['placeOfRegistrationCityId', new PropertyValidator(this.app, 'placeOfRegistrationCityId')],
-      ['placeOfRegistrationAddress', new PropertyValidator(this.app, 'placeOfRegistrationAddress', /[A-Za-z ]*/)],
-      ['homePhoneNumber', new PropertyValidator(this.app, 'homePhoneNumber', /^(\+(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*)?$/)],
-      ['mobilePhoneNumber', new PropertyValidator(this.app, 'mobilePhoneNumber', /^(\+(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*)?$/)],
-      ['email', new PropertyValidator(this.app, 'email', /^((([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,}))?$/)],
-      ['company', new PropertyValidator(this.app, 'company', /^[A-Za-z ]*$/)],
-      ['position', new PropertyValidator(this.app, 'position', /^[A-Za-z ]*$/)],
-      ['maritalStatusId', new PropertyValidator(this.app, 'maritalStatusId')],
-      ['citizenshipId', new PropertyValidator(this.app, 'citizenshipId')],
-      ['disabilityId', new PropertyValidator(this.app, 'disabilityId')],
-      ['isRetired', new PropertyValidator(this.app, 'isRetired')],
-      ['amount', new PropertyValidator(this.app, 'amount', /^\d*$/)],
-      ['isLiableForMilitaryService', new PropertyValidator(this.app, 'isLiableForMilitaryService')],    
+      ['lastName', new PropertyValidator(this.customersPage, 'lastName', /^([A-Z][a-z]*[\'\- ]?[A-Za-z]+)?$/)],
+      ['firstName', new PropertyValidator(this.customersPage, 'firstName', /^([A-Z][a-z]*[\'\- ]?[A-Za-z]+)?$/)],
+      ['middleName', new PropertyValidator(this.customersPage, 'middleName', /^([A-Z][a-z]*[\'\- ]?[A-Za-z]+)?$/)],
+      ['dateOfBirth', new PropertyValidator(this.customersPage, 'dateOfBirth', /^\d{4}-\d{2}-\d{2}$/)],
+      ['gender', new PropertyValidator(this.customersPage, 'gender')],
+      ['series', new PropertyValidator(this.customersPage, 'series', /^([A-Z]{2})?$/)],
+      ['passportNumber', new PropertyValidator(this.customersPage, 'passportNumber', /^(\d{7})?$/)],
+      ['issuingAuthority', new PropertyValidator(this.customersPage, 'issuingAuthority', /^[A-Za-z' ]*$/)],
+      ['issuedAt', new PropertyValidator(this.customersPage, 'issuedAt', /^\d{4}-\d{2}-\d{2}$/)],
+      ['idNumber', new PropertyValidator(this.customersPage, 'idNumber', /^([A-Z0-9]{14})?$/)],
+      ['placeOfBirth', new PropertyValidator(this.customersPage, 'placeOfBirth', /^[A-Za-z ]*$/)],
+      ['placeOfLivingCityId', new PropertyValidator(this.customersPage, 'placeOfLivingCityId')],
+      ['placeOfLivingAddress', new PropertyValidator(this.customersPage, 'placeOfLivingAddress')],
+      ['placeOfRegistrationCityId', new PropertyValidator(this.customersPage, 'placeOfRegistrationCityId')],
+      ['placeOfRegistrationAddress', new PropertyValidator(this.customersPage, 'placeOfRegistrationAddress', /[A-Za-z ]*/)],
+      ['homePhoneNumber', new PropertyValidator(this.customersPage, 'homePhoneNumber', /^(\+(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*)?$/)],
+      ['mobilePhoneNumber', new PropertyValidator(this.customersPage, 'mobilePhoneNumber', /^(\+(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*)?$/)],
+      ['email', new PropertyValidator(this.customersPage, 'email', /^((([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,}))?$/)],
+      ['company', new PropertyValidator(this.customersPage, 'company', /^[A-Za-z ]*$/)],
+      ['position', new PropertyValidator(this.customersPage, 'position', /^[A-Za-z ]*$/)],
+      ['maritalStatusId', new PropertyValidator(this.customersPage, 'maritalStatusId')],
+      ['citizenshipId', new PropertyValidator(this.customersPage, 'citizenshipId')],
+      ['disabilityId', new PropertyValidator(this.customersPage, 'disabilityId')],
+      ['isRetired', new PropertyValidator(this.customersPage, 'isRetired')],
+      ['amount', new PropertyValidator(this.customersPage, 'amount', /^\d*$/)],
+      ['isLiableForMilitaryService', new PropertyValidator(this.customersPage, 'isLiableForMilitaryService')],    
     ]))
   }
 
