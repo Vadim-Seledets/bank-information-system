5aeec3a5447fe74fbf918eaa4d3b0305e76106f7
Vadim-Seledets <Vadik446s@gmail.com>
2020-02-07 22:35:14 +0300

    Cosmetics

--- source/front/src/panels/customers/CustomerInfoPage.css.ts
+++ source/front/src/panels/customers/CustomerInfoPage.css.ts
@@ -27,20 +27,37 @@ export const style = restyle(() => {
 
     customerInfo: css`
       display: flex;
-      flex-direction: column;
+      flex-wrap: wrap;
+      justify-content: space-between;
       color: ${theme.applicationForeground};
       background-color: ${theme.applicationBackground};
     `,
 
+    propertyGroup: css`
+      display: flex;
+      flex-direction: column;
+      padding: 0.5em;
+      margin: 1em;
+      border-radius: 0.2em;
+      color: ${theme.customerInfoBubbleForeground};
+      background-color: ${theme.customerInfoBubbleBackground};
+
+      .hLine {
+        border-bottom: 1px solid ${theme.customerListRowBottomBorder};
+        margin: 0.3em 0.25em 0.5em 0.25em;
+      }
+    `,
+    
     propertyGroupCaption: css`
       font-size: 0.9em;
       font-weight: bold;
       padding: 0.5em 0;
+      color: ${theme.customerInfoBubbleCaptionForeground}; 
     `,
 
     property: css`
       display: flex;
-      padding: 0.25em 1em;
+      padding: 0.25em 0;
       font-size: 0.8em;
 
       .name {
@@ -49,6 +66,7 @@ export const style = restyle(() => {
 
       .value {
         margin-left: 1em;
+        color: ${theme.customerInfoBubbleCaptionForeground};
       }
     `,
   }
