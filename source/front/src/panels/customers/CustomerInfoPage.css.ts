29e3dc0452496030d68bc0e0e5ea44136088146a
Vadim-Seledets <Vadik446s@gmail.com>
2020-02-07 20:36:45 +0300

    Cosmetics

--- source/front/src/panels/customers/CustomerInfoPage.css.ts
+++ source/front/src/panels/customers/CustomerInfoPage.css.ts
@@ -28,6 +28,8 @@ export const style = restyle(() => {
     customerInfo: css`
       display: flex;
       flex-direction: column;
+      color: ${theme.applicationForeground};
+      background-color: ${theme.applicationBackground};
     `,
 
     propertyGroupCaption: css`
@@ -38,73 +40,15 @@ export const style = restyle(() => {
 
     property: css`
       display: flex;
-      flex-direction: column;
-      padding: 0.25em 0;
-      
-      .propertyNameLine {
-        display: flex;
-        align-items: baseline;
-        line-height: 1.1em;
+      padding: 0.25em 1em;
+      font-size: 0.8em;
 
-        .name {
-          font-size: 0.7em;
-          color: grey;
-        }
-
-        .error {
-          font-size: 0.9em;
-          color: red;
-
-          &:hover {
-            .errorPopUp {
-              display: flex;
-            }
-          }
-
-          .errorPopUp {
-            z-index: 3;
-            display: none;
-            position: absolute;
-            margin-left: -2em;
-            flex-direction: column;
-            font-size: 0.8em;
-            background-color: ${theme.applicationBackground};
-            box-shadow: 0 0 0.4em ${theme.applicationForeground}; 
-            border-radius: 0.3em;
-            
-            .errorRow { 
-              padding: 0.25em;
-            }
-          }
-        }
-
-        .input {
-          margin-left: 1em;
-          height: 1em;
-        }
+      .name {
+        
       }
 
-      .propertyInputColumn {
-        height: 1em;
-        font-size: 0.8em;
-        padding: 0.25em 0;
-        border-bottom: 1px solid grey;
-        color: ${theme.applicationForeground};
-        background-color: ${theme.applicationBackground};
-
-        &[is-invalid=true] {
-          border-bottom: 1px solid red;
-        }
-      }
-
-      .propertyInputHorizontalLine {
-        display: flex;
-        align-items: baseline;
-        height: 1em;
-        font-size: 0.8em;
-        padding: 0.25em 0;
-        color: ${theme.applicationForeground};
-        background-color: ${theme.applicationBackground};
+      .value {
+        margin-left: 1em;
       }
     `,
   }
