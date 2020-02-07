bbd6ad8e41387626973139a92b9ee6e95d68ab7f
Vadim-Seledets <Vadik446s@gmail.com>
2020-02-07 20:29:37 +0300

    Add CustomerInfoPage content

--- source/front/src/panels/customers/CustomerInfoPage.css.ts
+++ source/front/src/panels/customers/CustomerInfoPage.css.ts
@@ -20,19 +20,92 @@ export const style = restyle(() => {
       }
     `,
 
-    editOrPublishButton: css`
-      color: ${theme.sidebarSelectedTabForeground};
-      background-color: ${theme.sidebarSelectedTabBackground};
-
-      &[is-enabled=false] {
-        pointer-events: none;
-        background-color: grey;
-      }
-    `,
-
     backButton: css`
       color: ${theme.sidebarSelectedTabForeground};
       background-color: ${theme.sidebarSelectedTabBackground};
+    `,
+
+    customerInfo: css`
+      display: flex;
+      flex-direction: column;
+    `,
+
+    propertyGroupCaption: css`
+      font-size: 0.9em;
+      font-weight: bold;
+      padding: 0.5em 0;
+    `,
+
+    property: css`
+      display: flex;
+      flex-direction: column;
+      padding: 0.25em 0;
+      
+      .propertyNameLine {
+        display: flex;
+        align-items: baseline;
+        line-height: 1.1em;
+
+        .name {
+          font-size: 0.7em;
+          color: grey;
+        }
+
+        .error {
+          font-size: 0.9em;
+          color: red;
+
+          &:hover {
+            .errorPopUp {
+              display: flex;
+            }
+          }
+
+          .errorPopUp {
+            z-index: 3;
+            display: none;
+            position: absolute;
+            margin-left: -2em;
+            flex-direction: column;
+            font-size: 0.8em;
+            background-color: ${theme.applicationBackground};
+            box-shadow: 0 0 0.4em ${theme.applicationForeground}; 
+            border-radius: 0.3em;
+            
+            .errorRow { 
+              padding: 0.25em;
+            }
+          }
+        }
+
+        .input {
+          margin-left: 1em;
+          height: 1em;
+        }
+      }
+
+      .propertyInputColumn {
+        height: 1em;
+        font-size: 0.8em;
+        padding: 0.25em 0;
+        border-bottom: 1px solid grey;
+        color: ${theme.applicationForeground};
+        background-color: ${theme.applicationBackground};
+
+        &[is-invalid=true] {
+          border-bottom: 1px solid red;
+        }
+      }
+
+      .propertyInputHorizontalLine {
+        display: flex;
+        align-items: baseline;
+        height: 1em;
+        font-size: 0.8em;
+        padding: 0.25em 0;
+        color: ${theme.applicationForeground};
+        background-color: ${theme.applicationBackground};
+      }
     `,
   }
 })
--- source/front/src/panels/customers/CustomerInfoPage.tsx
+++ source/front/src/panels/customers/CustomerInfoPage.tsx
@@ -7,6 +7,7 @@ import { style } from './CustomerInfoPage.css'
 export function CustomerInfoPageView(p: { app: App }): JSX.Element {
   return reactive(() => {
     const css = style.classes
+    const customer = p.app.selectedCustomer
     return (
       <div className={css.main}>
         <div className={css.headLine} style={{ ...dim(2, 1, 11, 1) }}>
@@ -16,7 +17,121 @@ export function CustomerInfoPageView(p: { app: App }): JSX.Element {
           </button>
         </div>
         <div style={{ ...dim(2, 2, 11, 12) }}>
-          
+          {customer && (
+            <div className={css.customerInfo}>
+              <div className={css.propertyGroupCaption}>Personal Information</div>
+              <div className={css.property}>
+                <div className='name'>Last name</div>
+                <div className={'value'}>{customer.lastName}</div>
+              </div>
+              <div className={css.property}>
+                <div className='name'>First name</div>
+                <div className={'value'}>{customer.firstName}</div>
+              </div>
+              <div className={css.property}>
+                <div className='name'>Middle name</div>
+                <div className={'value'}>{customer.middleName}</div>
+              </div>
+              <div className={css.property}>
+                <div className='name'>Date of birth</div>
+                <div className={'value'}>{customer.dateOfBirth}</div>
+              </div>
+              <div className={css.property}>
+                <div className='name'>Gender</div>
+                <div className={'value'}>{customer.gender}</div>
+              </div>
+              <div className={css.propertyGroupCaption}>Passport Information</div>
+              <div className={css.property}>
+                <div className='name'>Citizenship</div>
+                {/* {p.customerInfo.auxiliary.countriesOfCitizenship.map((v, i) => (
+                    <option key={`${v.id}:${v.country}`} value={v.id}>{v.country}</option>
+                  ))} */}
+                <div className={'value'}>[Citizenship]</div>
+              </div>
+              <div className={css.property}>
+                <div className='name'>Passport series</div>
+                <div className={'value'}>{customer.series}</div>
+              </div>
+              <div className={css.property}>
+                <div className='name'>Passport number</div>
+                <div className={'value'}>{customer.passportNumber}</div>
+              </div>
+              <div className={css.property}>
+                <div className='name'>Issuing authority</div>
+                <div className={'value'}>{customer.issuingAuthority}</div>
+              </div>
+              <div className={css.property}>
+                <div className='name'>Issued at</div>
+                <div className={'value'}>{customer.issuedAt}</div>
+              </div>
+              <div className={css.property}>
+                <div className='name'>Id number</div>
+                <div className={'value'}>{customer.idNumber}</div>
+              </div>
+              <div className={css.propertyGroupCaption}>Contact Information</div>
+              <div className={css.property}>
+                <div className='name'>Place of birth</div>
+                <div className={'value'}>{customer.placeOfBirth}</div>
+              </div>
+              <div className={css.property}>
+                <div className='name'>Place of living</div>
+                <div className={'value'}>[City]</div>
+              </div>
+              <div className={css.property}>
+                <div className='name'>Address of living</div>
+                <div className={'value'}>{customer.placeOfLivingAddress}</div>
+              </div>
+              <div className={css.property}>
+                <div className='name'>Place of registration</div>
+                <div className={'value'}>[City]</div>
+              </div>
+              <div className={css.property}>
+                <div className='name'>Address of registration</div>
+                <div className={'value'}>{customer.placeOfRegistrationAddress}</div>
+              </div>
+              <div className={css.property}>
+                <div className='name'>Home phone number</div>
+                <div className={'value'}>{customer.homePhoneNumber}</div>
+              </div>
+              <div className={css.property}>
+                <div className='name'>Mobile phone number</div>
+                <div className={'value'}>{customer.mobilePhoneNumber}</div>
+              </div>
+              <div className={css.property}>
+                <div className='name'>Email</div>
+                <div className={'value'}>{customer.email}</div>
+              </div>
+              <div className={css.propertyGroupCaption}>Job Information</div>
+              <div className={css.property}>
+                <div className='name'>Company</div>
+                <div className={'value'}>{customer.company}</div>
+              </div>
+              <div className={css.property}>
+                <div className='name'>Position</div>
+                <div className={'value'}>{customer.position}</div>
+              </div>
+              <div className={css.property}>
+                <div className='name'>Salary</div>
+                <div className={'value'}>{`${customer.amount} ${customer.currencyId}`}</div>
+              </div>
+              <div className={css.property}>
+                <div className='name'>Marital status</div>
+                <div className={'value'}>{customer.maritalStatusId}</div>
+              </div>
+              <div className={css.property}>
+                <div className='name'>Disability</div>
+                <div className={'value'}>{customer.disabilityId}</div>
+              </div>
+              <div className={css.property}>
+                <div className='name'>Is retired</div>
+                <div className={'value'}>{customer.isRetired ? 'Yes' : 'No'}</div>
+              </div>
+              <div className={css.property}>
+                <div className='name'>Is liable for military service</div>
+                <div className={'value'}>{customer.isLiableForMilitaryService ? 'Yes' : 'No'}</div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     )
