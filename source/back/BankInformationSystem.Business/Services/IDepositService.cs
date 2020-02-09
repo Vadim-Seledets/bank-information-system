09f5788cba8eaaa0de46516ceebab7e87cf5fde2
Arseni Rynkevich <rynkevich@icloud.com>
2020-02-09 10:41:48 -0800

    Add separate service and controller for deposits

---
+++ source/back/BankInformationSystem.Business/Services/IDepositService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BankInformationSystem.Business.Models;
+
+namespace BankInformationSystem.Business.Services
+{
+    public interface IDepositService
+    {
+        Task<IList<DepositContractShortInfoModel>> GetDepositContractsAsync();
+
+        Task<DepositContractDetailsModel> GetDepositContractDetailsAsync(Guid contractNumber);
+
+        Task OpenDepositAsync(DepositCreateModel model);
+
+        Task RevokeDepositAsync(Guid contractNumber);
+    }
+}
--- source/back/BankInformationSystem/Controllers/BankOperationsController.cs
+++ source/back/BankInformationSystem/Controllers/BankOperationsController.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using BankInformationSystem.Business.Models;
 using BankInformationSystem.Business.Services;
@@ -20,58 +19,6 @@ namespace BankInformationSystem.Controllers
             _operationsService = operationsService;
         }
 
-        [HttpGet]
-        [Route("deposits")]
-        [ProducesResponseType(typeof(IEnumerable<DepositContractShortInfoModel>), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<IEnumerable<DepositContractShortInfoModel>>> GetDepositsAsync()
-        {
-            var customers = await _operationsService.GetDepositContractsAsync();
-
-            return Ok(customers);
-        }
-        
-        [HttpGet]
-        [Route("deposits/{number}")]
-        [ProducesResponseType(typeof(DepositContractDetailsModel), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<DepositContractDetailsModel>> GetDepositContractByNumberAsync(Guid number)
-        {
-            var contract = await _operationsService.GetDepositContractDetailsAsync(number);
-
-            if (contract == null)
-            {
-                return NotFound();
-            }
-            
-            return Ok(contract);
-        }
-
-        [HttpPost]
-        [Route("deposits")]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [ProducesResponseType(typeof(ApiErrorModel), StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<Guid>> OpenDepositAsync(DepositCreateModel model)
-        {
-            await _operationsService.OpenDepositAsync(model);
-            
-            return NoContent();
-        }
-        
-        [HttpPost]
-        [Route("deposits/{number}/revoke")]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [ProducesResponseType(typeof(ApiErrorModel), StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<Guid>> RevokeDepositAsync(Guid number)
-        {
-            await _operationsService.RevokeDepositAsync(number);
-            
-            return NoContent();
-        }
-        
         [HttpPost]
         [Route("commit")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
