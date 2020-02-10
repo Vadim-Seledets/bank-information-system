0ef7159236353fdfb55c5e0f8a68838a59456f71
Vadim-Seledets <Vadik446s@gmail.com>
2020-02-10 13:47:03 +0300

    Fix regexp for guid creation

--- source/front/src/models/deposits/DepositCreationPage.ts
+++ source/front/src/models/deposits/DepositCreationPage.ts
@@ -45,7 +45,7 @@ export class DepositCreationPage extends Stateful {
   }
 
   private generateGuid(): string {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/, c => {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
       const r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8)
       return v.toString(16)
     })
