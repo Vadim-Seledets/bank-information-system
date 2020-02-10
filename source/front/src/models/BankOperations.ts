f8a094fb70c6a40aaef3aa73c250691b6624a976
Vadim-Seledets <Vadik446s@gmail.com>
2020-02-10 20:36:19 +0300

    Move Auxiliary to BankOperations

--- source/front/src/models/BankOperations.ts
+++ source/front/src/models/BankOperations.ts
@@ -1,3 +1,5 @@
+import { Stateful, action } from "reactronic"
+
 export class Auxiliary {
   cities: Array<{id: number, name: string}> = []
   countriesOfCitizenship: Array<{id: number, country: string}> = []
@@ -5,4 +7,18 @@ export class Auxiliary {
   currencies: Array<{id: number, code: string}> = []
   maritalStatuses: Array<{id: number, description: string}> = []
   depositTypes:  Array<{id: number, name: string}> = []
+}
+
+export class CloseBankDayData extends Stateful {
+  times: number
+
+  constructor(times: number) {
+    super()
+    this.times = times
+  }
+  
+  @action
+  setTimes(value: number): void {
+    this.times = value
+  }
 }
