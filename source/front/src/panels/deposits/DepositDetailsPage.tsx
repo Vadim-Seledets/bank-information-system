f541225e9bd761f30538cc18efed1c4bb7fc71dc
Vadim-Seledets <Vadik446s@gmail.com>
2020-02-10 22:08:33 +0300

    Cosmetics

--- source/front/src/panels/deposits/DepositDetailsPage.tsx
+++ source/front/src/panels/deposits/DepositDetailsPage.tsx
@@ -116,10 +116,10 @@ export function DepositDetailsPageView(p: { depositsPage: DepositsPage }): JSX.E
                 {depositDetailes.transactions.map((t, i) => (
                   <React.Fragment key={i}>
                     <div style={{ ...dim(1, i + 2, 1, i + 2) }}>{t.createdAt}</div>
-                    <div style={{ ...dim(3, i + 2, 3, i + 2) }}>{`${t.amount} ${auxiliary.currencies.find(v => v.id === t.currencyId)?.code}`}</div>
-                    <div style={{ ...dim(5, i + 2, 5, i + 2) }}>{t.senderAccountNumber}</div>
+                    <div style={{ ...dim(3, i + 2, 3, i + 2) }}>{`${t.amount.toFixed(4)} ${auxiliary.currencies.find(v => v.id === t.currencyId)?.code}`}</div>
+                    <div style={{ ...dim(5, i + 2, 5, i + 2), color: `hsl(${parseInt(t.senderAccountNumber?.substr(0, 2)) + parseInt(t.senderAccountNumber?.substr(11, 2))}, 100%, 50%)` }}>{t.senderAccountNumber}</div>
                     <div style={{ ...dim(6, i + 2, 6, i + 2) }} className='las la-long-arrow-alt-right' />
-                    <div style={{ ...dim(7, i + 2, 7, i + 2) }}>{t.receiverAccountNumber}</div>
+                    <div style={{ ...dim(7, i + 2, 7, i + 2), color: `hsl(${parseInt(t.receiverAccountNumber?.substr(0, 2)) + parseInt(t.receiverAccountNumber.substr(11, 2))}, 100%, 50%)` }}>{t.receiverAccountNumber}</div>
                     <div style={{ ...dim(1, i + 2, 7, i + 2) }} className={css.transactionRow} />
                   </React.Fragment>
                 ))}
