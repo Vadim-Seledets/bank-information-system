3c478604b70fda0de59a5d7b222a995f2e1efaa8
Vadim-Seledets <Vadik446s@gmail.com>
2020-02-13 23:09:29 +0300

    Make error messages beautiful

--- source/front/src/models/ApiErrors.ts
+++ source/front/src/models/ApiErrors.ts
@@ -24,7 +24,12 @@ export class ApiErrors extends Stateful {
   getPropertyErrors(propertyName: string): Array<string> {
     let errors = new Array<string>()
     if (this.errors) {
-      errors = this.errors.filter(v => v.name === propertyName).map(v => v.message)
+      errors = this.errors.filter(v => v.name === propertyName).map(v => {
+        const messageWithoutName = v.message.replace(/^'.*' /, '')
+        const fancyMessage = messageWithoutName.charAt(0).toUpperCase()
+          + messageWithoutName.slice(1, messageWithoutName.length - 1)
+        return fancyMessage
+      })
     }
     return errors
   }
