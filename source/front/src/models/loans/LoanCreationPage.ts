6c985faed9e55965010c3a56586886d29fdd0e60
Vadim-Seledets <Vadik446s@gmail.com>
2020-02-15 10:32:51 +0300

    Add LoanCreationPage

---
+++ source/front/src/models/loans/LoanCreationPage.ts
@@ -0,0 +1,74 @@
+import { Stateful, action } from "reactronic"
+import { CreatingLoan, LoanCreateModel } from "./Loan"
+import { LoansPage } from "./LoansPage"
+import { IApiErrors, ApiErrors } from "../ApiErrors"
+import { Validation, PropertyValidator } from "../Validation"
+
+export class LoanCreationPage extends Stateful {
+  apiErrors: ApiErrors | undefined
+  loansPage: LoansPage
+  creatingLoan: CreatingLoan | undefined
+  validation: Validation<LoanCreateModel>
+
+  constructor(loansPage: LoansPage) {
+    super()
+    this.apiErrors = undefined
+    this.loansPage = loansPage
+    this.creatingLoan = undefined
+    this.validation = new Validation(
+      new Map([
+        ['contractNumber', new PropertyValidator<LoanCreateModel>('contractNumber')],
+        ['programStartDate', new PropertyValidator<LoanCreateModel>('programStartDate', /^\d{4}-\d{2}-\d{2}$/)],
+        ['programEndDate', new PropertyValidator<LoanCreateModel>('programEndDate', /^\d{4}-\d{2}-\d{2}$/)],
+        ['contractValidUntil', new PropertyValidator<LoanCreateModel>('contractValidUntil', /^\d{4}-\d{2}-\d{2}$/)],
+        ['customerId', new PropertyValidator<LoanCreateModel>('customerId')],
+        ['amount', new PropertyValidator<LoanCreateModel>('amount', /^\d{1,10}$/)],
+        ['rate', new PropertyValidator<LoanCreateModel>('rate', /^(0[.,][0-9]+)|1$/)],
+        ['currencyId', new PropertyValidator<LoanCreateModel>('currencyId')],
+      ])
+    )
+  }
+
+  @action
+  createNewLoan(): void {
+    this.creatingLoan = new CreatingLoan(this.generateGuid())
+  }
+
+  @action
+  setCreatingLoan(loan: CreatingLoan | undefined): void {
+    this.creatingLoan = loan
+  }
+
+  @action
+  cancelCreation(): void {
+    this.setApiErrors(undefined)
+    this.setCreatingLoan(undefined)
+    this.loansPage.app.currentTab?.setCurrentPageName('LoansListPage')
+  }
+
+  @action
+  setApiErrors(apiErrors: IApiErrors | undefined): void {
+    this.apiErrors = apiErrors ? new ApiErrors(apiErrors) : undefined
+  }
+
+  @action
+  async publishNewLoanRequest(): Promise<void> {
+    this.setApiErrors(undefined)
+    if (this.creatingLoan) {
+      const url = `https://localhost:5001/loans`
+      await this.loansPage.app.httpClient.post(url, this.creatingLoan.getJson())
+      const errors = this.loansPage.app.httpClient.getAndDeleteLastError<IApiErrors>('POST', url)
+      this.setApiErrors(errors)
+      if (this.apiErrors === undefined) {
+        this.loansPage.app.currentTab?.setCurrentPageName('LoansListPage')
+      }
+    }
+  }
+
+  private generateGuid(): string {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+      const r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8)
+      return v.toString(16)
+    })
+  }
+}
---
+++ source/front/src/panels/loans/LoanCreationPage.tsx
@@ -0,0 +1,112 @@
+import * as React from 'react'
+import { reactive } from 'reactronic-toolkit-react'
+import { cx } from 'emotion'
+import { dim } from '../../common/css'
+import { style } from './LoanCreationPage.css'
+import { commonStyle } from '../../common/CommonStyles.css'
+import { LoansPage } from '../../models/loans/LoansPage'
+
+export function LoanCreationPageView(p: { loansPage: LoansPage }): JSX.Element {
+  return reactive(() => {
+    const css = style.classes
+    const commonCss = commonStyle.classes
+    const creatingLoan = p.loansPage.loanCreationPage.creatingLoan
+    const auxiliary = p.loansPage.app.auxiliary
+    const apiErrors = p.loansPage.loanCreationPage.apiErrors
+    return (
+      <div className={commonCss.main}>
+        <div className={commonCss.headLine} style={{ ...dim(2, 1, 11, 1) }}>
+          <button className={css.editOrPublishButton}
+            is-enabled={`${p.loansPage.loanCreationPage.validation.areAllValid(creatingLoan!)}`}
+            onClick={() => p.loansPage.loanCreationPage.publishNewLoanRequest()}
+          >
+            <React.Fragment>
+              <span className='las la-upload' style={{ marginRight: '0.5em' }} />
+              Publish a New Loan
+            </React.Fragment>
+          </button>
+          <div className='space' />
+          <button className={commonCss.backButton} onClick={() => p.loansPage.loanCreationPage.cancelCreation()}>
+            Cancel
+          </button>
+        </div>
+        <div className={css.properties} style={{ ...dim(2, 2, 11, 12) }}>
+          {creatingLoan && (
+            <React.Fragment>
+              <div style={{ ...dim(1, 1, 5, 1) }} className={css.header}>
+                <div className='caption'>Loan Information</div>
+                {apiErrors?.has('') && <div style={{ ...dim(3, 1, 5, 1) }} className={css.error}>{apiErrors?.getPropertyErrors('')[0]}</div>}
+              </div>
+
+              <div style={{ ...dim(1, 3, 1, 3) }} className={css.caption}>Contract Number</div>
+              <input style={{ ...dim(3, 3, 3, 3) }} className={css.input} disabled={true} type="text" value={creatingLoan.contractNumber}
+                onFocus={() => apiErrors?.deleteError('ContractNumber')}
+              />
+              {apiErrors?.has('ContractNumber') && <div style={{ ...dim(5, 3, 5, 3) }} className={css.error}>{apiErrors?.getPropertyErrors('ContractNumber')[0]}</div>}
+
+              <div style={{ ...dim(1, 4, 1, 4) }} className={css.caption}>Program Start Date</div>
+              <input style={{ ...dim(3, 4, 3, 4) }} className={css.input} type="date" value={creatingLoan.programStartDate}
+                is-invalid={`${!p.loansPage.loanCreationPage.validation.isValid(creatingLoan, 'programStartDate') || apiErrors?.has('ProgramStartDate')}`}
+                onFocus={() => apiErrors?.deleteError('ProgramStartDate')}
+                onChange={e => creatingLoan.setProgramStartDate(e.currentTarget.value)}
+              />
+              {apiErrors?.has('ProgramStartDate') && <div style={{ ...dim(5, 4, 5, 4) }} className={css.error}>{apiErrors?.getPropertyErrors('ProgramStartDate')[0]}</div>}
+
+              <div style={{ ...dim(1, 5, 1, 5) }} className={css.caption}>Program End Date</div>
+              <input style={{ ...dim(3, 5, 3, 5) }} className={css.input} type="date" value={creatingLoan.programEndDate}
+                is-invalid={`${!p.loansPage.loanCreationPage.validation.isValid(creatingLoan, 'programEndDate') || apiErrors?.has('ProgramEndDate')}`}
+                onFocus={() => apiErrors?.deleteError('ProgramEndDate')}
+                onChange={e => creatingLoan.setProgramEndDate(e.currentTarget.value)}
+              />
+              {apiErrors?.has('ProgramEndDate') && <div style={{ ...dim(5, 5, 5, 5) }} className={css.error}>{apiErrors?.getPropertyErrors('ProgramEndDate')[0]}</div>}
+
+              <div style={{ ...dim(1, 6, 1, 6) }} className={css.caption}>Contract Valid Until</div>
+              <input style={{ ...dim(3, 6, 3, 6) }} className={css.input} type="date" value={creatingLoan.contractValidUntil}
+                is-invalid={`${!p.loansPage.loanCreationPage.validation.isValid(creatingLoan, 'contractValidUntil') || apiErrors?.has('ContractValidUntil')}`}
+                onFocus={() => apiErrors?.deleteError('ContractValidUntil')}
+                onChange={e => creatingLoan.setContractValidUntil(e.currentTarget.value)}
+              />
+              {apiErrors?.has('ContractValidUntil') && <div style={{ ...dim(5, 6, 5, 6) }} className={css.error}>{apiErrors?.getPropertyErrors('ContractValidUntil')[0]}</div>}
+
+              <div style={{ ...dim(1, 7, 1, 7) }} className={css.caption}>Customer</div>
+              <select style={{ ...dim(3, 7, 3, 7) }} className={css.input} value={creatingLoan.customerId}
+                is-invalid={`${!p.loansPage.loanCreationPage.validation.isValid(creatingLoan, 'customerId') || apiErrors?.has('CustomerId')}`}
+                onChange={e => creatingLoan.setCustomerId(parseInt(e.currentTarget.value))}
+              >
+                {p.loansPage.app.customersPage.customers.map((v, i) => (
+                  <option key={`${v.id}`} value={v.id}>{`${v.firstName} ${v.middleName} ${v.lastName}`}</option>
+                ))}
+              </select>
+              {apiErrors?.has('CustomerId') && <div style={{ ...dim(5, 7, 5, 7) }} className={css.error}>{apiErrors?.getPropertyErrors('CustomerId')[0]}</div>}
+
+              <div style={{ ...dim(1, 8, 1, 8) }} className={css.caption}>Amount</div>
+              <div style={{ ...dim(3, 8, 3, 8) }} className={cx(css.input, css.money)}
+                is-invalid={`${!p.loansPage.loanCreationPage.validation.isValid(creatingLoan, 'amount') || apiErrors?.has('Amount')}`}
+              >
+                <input className='amount' type="text" defaultValue={creatingLoan.amount}
+                  onChange={e => creatingLoan.setAmount(parseFloat(e.currentTarget.value))}
+                />
+                <select className='currency' defaultValue={creatingLoan.currencyId}
+                  onChange={e => creatingLoan.setCurrencyId(parseInt(e.currentTarget.value))}
+                >
+                  {auxiliary.currencies.map((v, i) => (
+                    <option key={`${v.id}:${v.code}`} value={v.id}>{v.code}</option>
+                  ))}
+                </select>
+              </div>
+              {apiErrors?.has('Amount') && <div style={{ ...dim(5, 8, 5, 8) }} className={css.error}>{apiErrors?.getPropertyErrors('Amount')[0]}</div>}
+
+              <div style={{ ...dim(1, 9, 1, 9) }} className={css.caption}>Rate</div>
+              <input style={{ ...dim(3, 9, 3, 9) }} className={css.input} type="number" max={1} min={0} step={0.001} defaultValue={creatingLoan.rate}
+                is-invalid={`${!p.loansPage.loanCreationPage.validation.isValid(creatingLoan, 'rate') || apiErrors?.has('Rate')}`}
+                onFocus={() => apiErrors?.deleteError('Rate')}
+                onChange={e => creatingLoan.setRate(e.currentTarget.value !== '' ? parseFloat(e.currentTarget.value) : 0)}
+              />
+              {apiErrors?.has('Rate') && <div style={{ ...dim(5, 9, 5, 9) }} className={css.error}>{apiErrors?.getPropertyErrors('Rate')[0]}</div>}
+            </React.Fragment>
+          )}
+        </div>
+      </div>
+    )
+  })
+}
