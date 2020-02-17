e21115b7ef5a4d12f92943605b6220bac19d4714
Vadim-Seledets <Vadik446s@gmail.com>
2020-02-17 20:59:20 +0300

    Get rid of unused code

--- source/front/src/panels/deposits/DepositsPage.css.ts
+++ source/front/src/panels/deposits/DepositsPage.css.ts
@@ -38,26 +38,13 @@ export const style = restyle(() => {
     `,
 
     depositList: css`
-      display: grid;
-      align-items: center;
-      justify-items: center;
       grid-template-columns: 1em 5fr 0.5fr 4fr 0.5fr 2fr 0.5fr 2fr 1em;
-      color: ${theme.applicationForeground};
-      font-size: 0.8em;
-      user-select: none;
 
       .icon {
         font-size: 1.2em;
-        z-index: 2;
-
-        &:hover {
-          z-index: 4;
-        }
       }
 
       .contractNumber {
-        z-index: 2;
-
         &:hover {
           color: ${theme.highlighter};
           cursor: pointer;
@@ -65,8 +52,6 @@ export const style = restyle(() => {
       }
 
       .customerFullName {
-        z-index: 2;
-
         mark {
           color: ${theme.markForeground};
           background-color: transparent;
@@ -74,56 +59,7 @@ export const style = restyle(() => {
       }
 
       .programDate {
-        z-index: 2;
-        /* color: #629D30; */
-      }
 
-      .errors {
-        color: red;
-
-        &:hover {
-          .errorsPopUp {
-            display: flex;
-          }
-        }
-
-        .errorsPopUp {
-          display: none;
-          position: absolute;
-          margin-bottom: -1em;
-          margin-left: -15em;
-          flex-direction: column;
-          font-size: 0.7em;
-          background-color: ${theme.applicationBackground};
-          box-shadow: 0 0 0.4em ${theme.applicationForeground}; 
-          border-radius: 0.3em;
-
-          .errorRow {
-            padding: 0.25em;
-          }
-        }
-      }
-
-      .row {
-        z-index: 1;
-        width: 100%;
-        padding: 1em 0;
-        border-bottom: 1px solid ${theme.customerListRowBottomBorder};
-        transition: background-color ease 0.2s;
-      }
-
-      .oddRow {
-        background-color: ${theme.customerListOddRowBackground};
-      }
-
-      .rowHighlighter {
-        &[is-hovered=true] {
-          background-color: ${theme.customerListHoveredRowBackground};
-        }
-        
-        &[is-selected=true] {
-          background-color: ${theme.customerListHighlightedRowBackground};
-        }
       }
     `,
   }
