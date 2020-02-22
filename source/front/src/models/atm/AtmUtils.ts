163127389f6133131e83486a09645676d32bd236
Vadim-Seledets <Vadik446s@gmail.com>
2020-02-22 18:15:54 +0300

    Fix hash algorithm

--- source/front/src/models/atm/AtmUtils.ts
+++ source/front/src/models/atm/AtmUtils.ts
@@ -2,6 +2,5 @@ export async function digestMessageInBase64(message: string): Promise<string> {
   const msgUint8 = new TextEncoder().encode(message)
   const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8)
   const hashArray = Array.from(new Uint8Array(hashBuffer))
-  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('')
-  return btoa(hashHex)
+  return btoa(String.fromCharCode.apply(null, hashArray))
 }
