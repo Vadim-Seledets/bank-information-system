fcb9d2250ad4eafdf3e3916e0d75d8dcf7bd745c
Vadim-Seledets <Vadik446s@gmail.com>
2020-02-23 18:21:28 +0300

    Get rid of 'search' style

--- source/front/src/panels/deposits/DepositsPage.css.ts
+++ source/front/src/panels/deposits/DepositsPage.css.ts
@@ -4,20 +4,7 @@ import { themes } from '../../models/Theme'
 
 export const style = restyle(() => {
   const theme = themes.active
-  return { 
-    search: css`
-      align-self: center;
-      justify-self: right;
-      display: flex;
-      align-items: center;
-      padding: 0.1em 0.3em;
-      color: ${theme.applicationForeground};
-      background-color: ${theme.searchInputBackground};
-      border-bottom: 1px solid ${theme.searchInputBorderColor};
-      border-radius: 0.2em;
-      font-size: 0.8em;
-    `,
-
+  return {
     button: css`
       justify-self: left;
       color: ${theme.sidebarSelectedTabForeground};
